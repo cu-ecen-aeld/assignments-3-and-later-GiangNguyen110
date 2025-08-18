8b68f85b28089d19e5f07bde90a23faf2a922806
GiangNguyen110 <nguyen.tgiang97@gmail.com>
2025-08-18 18:04:33 +0700

    Minor update

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,115 +13,115 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $# -lt 1 ]
-then
-	echo "Using default directory ${OUTDIR} for output"
-else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
-fi
+# if [ $# -lt 1 ]
+# then
+# 	echo "Using default directory ${OUTDIR} for output"
+# else
+# 	OUTDIR=$1
+# 	echo "Using passed directory ${OUTDIR} for output"
+# fi
 
-mkdir -p ${OUTDIR}
+# mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    # Clone only if the repository does not exist.
-    echo "===== Cloning Linux Repository ====="
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+# cd "$OUTDIR"
+# if [ ! -d "${OUTDIR}/linux-stable" ]; then
+#     # Clone only if the repository does not exist.
+#     echo "===== Cloning Linux Repository ====="
+# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+# fi
+# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+#     cd linux-stable
+#     echo "Checking out version ${KERNEL_VERSION}"
+#     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
-    echo "===== Building Kernel ====="
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
-    # make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
-fi
+#     # TODO: Add your kernel build steps here
+#     echo "===== Building Kernel ====="
+#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+#     # make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+# fi
 
-echo "Adding the Image in outdir"
-cp "${OUTDIR}/linux-stable/arch/arm64/boot/Image" "${OUTDIR}"
+# echo "Adding the Image in outdir"
+# cp "${OUTDIR}/linux-stable/arch/arm64/boot/Image" "${OUTDIR}"
 
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
+# echo "Creating the staging directory for the root filesystem"
+# cd "$OUTDIR"
+# if [ -d "${OUTDIR}/rootfs" ]
+# then
+# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+#     sudo rm  -rf ${OUTDIR}/rootfs
+# fi
 
-# TODO: Create necessary base directories
-mkdir -p ${OUTDIR}/rootfs/bin  ${OUTDIR}/rootfs/dev  ${OUTDIR}/rootfs/etc   \
-         ${OUTDIR}/rootfs/home ${OUTDIR}/rootfs/lib  ${OUTDIR}/rootfs/lib64 \
-         ${OUTDIR}/rootfs/proc ${OUTDIR}/rootfs/sbin ${OUTDIR}/rootfs/sys   \
-         ${OUTDIR}/rootfs/tmp  ${OUTDIR}/rootfs/usr  ${OUTDIR}/rootfs/var   \
-         ${OUTDIR}/rootfs/usr/bin ${OUTDIR}/rootfs/usr/lib ${OUTDIR}/rootfs/usr/sbin \
-         ${OUTDIR}/rootfs/var/log
+# # TODO: Create necessary base directories
+# mkdir -p ${OUTDIR}/rootfs/bin  ${OUTDIR}/rootfs/dev  ${OUTDIR}/rootfs/etc   \
+#          ${OUTDIR}/rootfs/home ${OUTDIR}/rootfs/lib  ${OUTDIR}/rootfs/lib64 \
+#          ${OUTDIR}/rootfs/proc ${OUTDIR}/rootfs/sbin ${OUTDIR}/rootfs/sys   \
+#          ${OUTDIR}/rootfs/tmp  ${OUTDIR}/rootfs/usr  ${OUTDIR}/rootfs/var   \
+#          ${OUTDIR}/rootfs/usr/bin ${OUTDIR}/rootfs/usr/lib ${OUTDIR}/rootfs/usr/sbin \
+#          ${OUTDIR}/rootfs/var/log
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-#     # TODO:  Configure busybox
-    make distclean
-    make defconfig
-else
-    cd busybox
-fi
+# cd "$OUTDIR"
+# if [ ! -d "${OUTDIR}/busybox" ]
+# then
+# git clone git://busybox.net/busybox.git
+#     cd busybox
+#     git checkout ${BUSYBOX_VERSION}
+# #     # TODO:  Configure busybox
+#     make distclean
+#     make defconfig
+# else
+#     cd busybox
+# fi
 
-# TODO: Make and install busybox
-echo "===== Make and install Busybox ====="
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+# # TODO: Make and install busybox
+# echo "===== Make and install Busybox ====="
+# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+# make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-# Make BusyBox binary setuid root
-chmod u+s ${OUTDIR}/rootfs/bin/busybox
+# # Make BusyBox binary setuid root
+# chmod u+s ${OUTDIR}/rootfs/bin/busybox
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
+# echo "Library dependencies"
+# ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+# ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
-echo "Add library dependencies to rootfs"
-SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+# # TODO: Add library dependencies to rootfs
+# echo "Add library dependencies to rootfs"
+# SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
-cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-cp -a ${SYSROOT}/lib64/libm.so.* ${OUTDIR}/rootfs/lib64
-cp -a ${SYSROOT}/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64
-cp -a ${SYSROOT}/lib64/libc.so.* ${OUTDIR}/rootfs/lib64
+# cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+# cp -a ${SYSROOT}/lib64/libm.so.* ${OUTDIR}/rootfs/lib64
+# cp -a ${SYSROOT}/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64
+# cp -a ${SYSROOT}/lib64/libc.so.* ${OUTDIR}/rootfs/lib64
 
-# TODO: Make device nodes
-echo "Make devie nodes"
+# # TODO: Make device nodes
+# echo "Make devie nodes"
 cd ${OUTDIR}/rootfs
-sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1
+# sudo mknod -m 666 dev/null c 1 3
+# sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
-echo "Clean and build the writer utility"
-export CROSS_COMPILE=${CROSS_COMPILE}
-make -C "${FINDER_APP_DIR}" clean
-make -C "${FINDER_APP_DIR}" all
+# # TODO: Clean and build the writer utility
+# echo "Clean and build the writer utility"
+# export CROSS_COMPILE=${CROSS_COMPILE}
+# make -C "${FINDER_APP_DIR}" clean
+# make -C "${FINDER_APP_DIR}" all
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 echo "Copy the finder related scripts and executables to the /home directory on the target rootfs"
-cp -r * ${OUTDIR}/rootfs/home
+cp -r ${FINDER_APP_DIR}/* ${OUTDIR}/rootfs/home
 cp -r ../conf ${OUTDIR}/rootfs
 
-# TODO: Chown the root directory
-echo "Chown the root directory"
-cd ${OUTDIR}/rootfs
-sudo chown -R root:root *
+# # TODO: Chown the root directory
+# echo "Chown the root directory"
+# cd ${OUTDIR}/rootfs
+# sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
-echo "===== Create initramfs.cpio.gz ====="
-cd ${OUTDIR}/rootfs
-find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-gzip -f ${OUTDIR}/initramfs.cpio
+# # TODO: Create initramfs.cpio.gz
+# echo "===== Create initramfs.cpio.gz ====="
+# cd ${OUTDIR}/rootfs
+# find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+# gzip -f ${OUTDIR}/initramfs.cpio
